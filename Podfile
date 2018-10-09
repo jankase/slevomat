469764578527e3295d6deb1a602ceb3e9337c5fe
Jan Kaše <jan.kase@me.com>
2018-10-09 20:00:33 +0200

    initial commit

---
+++ Podfile
@@ -0,0 +1,16 @@
+project 'SlevomatCS.xcodeproj'
+
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'SlevomatCS' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Alamofire'
+  pod 'RealmSwift'
+  pod 'SnapKit'
+
+  # Pods for SlevomatCS
+
+end
---
+++ SlevomatCS/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  SlevomatCS
+//
+//  Created by Jan Kase on 2018-10-09.
+//  Copyright © 2018 Jan Kaše. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+}
